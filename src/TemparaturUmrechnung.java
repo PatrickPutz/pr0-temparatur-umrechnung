08caecf48e8099a3ab6ff7159917542587ab4ced
pputz <putz.patrick@outlook.com>
2021-09-26 18:05:56 +0200

    implemented calculation from celsius to fahrenheit and vice versa

--- src/TemparaturUmrechnung.java
+++ src/TemparaturUmrechnung.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class TemparaturUmrechnung {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Celsius - Fahrenheit calculator!");
+
+        // ask for and scan temperature for calculation
+        System.out.print("Enter a value for calculation: ");
+        Scanner getTemperature = new Scanner(System.in);
+        double temperature = getTemperature.nextDouble();
+
+        // calculate temperature and print it
+        System.out.println(temperature + "° Fahrenheit = " + ((temperature -32) * 5 / 9) + "° Celsius!");
+        System.out.println(temperature + "° Celsius = " + (temperature * 1.8 + 32) + "° Fahrenheit!");
     }
 }
